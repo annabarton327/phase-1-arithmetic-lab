ab6f873c0c4a0be79bac47d909025da9dbd2e83f
Anna Barton <anna.barton327@gmail.com>
2023-01-30 20:26:14 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2 
+
+
+let random = Math.round(Math.random() * 10 + 1)
+
+
+const num3 = 14
+const num4 = 5
+const mod = num3 % num4
+
+
+
+
+const max = Math.max(1, 2, 20)
+
